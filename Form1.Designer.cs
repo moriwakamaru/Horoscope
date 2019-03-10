8c26d0c735db2dfaf6bf6402043c2ae3eb6329aa
waka <wa_morita@route-zero.com>
2019-03-10 21:55:29 +0900

    絵の未追加、テキスト内容未完成
    絵の未追加、テキスト内容未完成

---
+++ Form1.Designer.cs
@@ -0,0 +1,394 @@
+﻿namespace Horoscope
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonAries = new System.Windows.Forms.Button();
+            this.buttonTaurus = new System.Windows.Forms.Button();
+            this.buttonGemini = new System.Windows.Forms.Button();
+            this.buttonCancer = new System.Windows.Forms.Button();
+            this.buttonLeo = new System.Windows.Forms.Button();
+            this.buttonVirgo = new System.Windows.Forms.Button();
+            this.buttonLibra = new System.Windows.Forms.Button();
+            this.buttonScorpio = new System.Windows.Forms.Button();
+            this.buttonSagittarius = new System.Windows.Forms.Button();
+            this.buttonCapricorn = new System.Windows.Forms.Button();
+            this.buttonAquarius = new System.Windows.Forms.Button();
+            this.buttonPisces = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.pictureBox12 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonAries
+            // 
+            this.buttonAries.Location = new System.Drawing.Point(22, 68);
+            this.buttonAries.Name = "buttonAries";
+            this.buttonAries.Size = new System.Drawing.Size(75, 23);
+            this.buttonAries.TabIndex = 0;
+            this.buttonAries.Text = "おひつじ座";
+            this.buttonAries.UseVisualStyleBackColor = true;
+            this.buttonAries.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonTaurus
+            // 
+            this.buttonTaurus.Location = new System.Drawing.Point(142, 68);
+            this.buttonTaurus.Name = "buttonTaurus";
+            this.buttonTaurus.Size = new System.Drawing.Size(75, 23);
+            this.buttonTaurus.TabIndex = 1;
+            this.buttonTaurus.Text = "おうし座";
+            this.buttonTaurus.UseVisualStyleBackColor = true;
+            this.buttonTaurus.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // buttonGemini
+            // 
+            this.buttonGemini.Location = new System.Drawing.Point(257, 68);
+            this.buttonGemini.Name = "buttonGemini";
+            this.buttonGemini.Size = new System.Drawing.Size(75, 23);
+            this.buttonGemini.TabIndex = 2;
+            this.buttonGemini.Text = "ふたご座";
+            this.buttonGemini.UseVisualStyleBackColor = true;
+            this.buttonGemini.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // buttonCancer
+            // 
+            this.buttonCancer.Location = new System.Drawing.Point(375, 68);
+            this.buttonCancer.Name = "buttonCancer";
+            this.buttonCancer.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancer.TabIndex = 3;
+            this.buttonCancer.Text = "かに座";
+            this.buttonCancer.UseVisualStyleBackColor = true;
+            this.buttonCancer.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // buttonLeo
+            // 
+            this.buttonLeo.Location = new System.Drawing.Point(22, 162);
+            this.buttonLeo.Name = "buttonLeo";
+            this.buttonLeo.Size = new System.Drawing.Size(75, 23);
+            this.buttonLeo.TabIndex = 4;
+            this.buttonLeo.Text = "しし座";
+            this.buttonLeo.UseVisualStyleBackColor = true;
+            this.buttonLeo.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // buttonVirgo
+            // 
+            this.buttonVirgo.Location = new System.Drawing.Point(142, 162);
+            this.buttonVirgo.Name = "buttonVirgo";
+            this.buttonVirgo.Size = new System.Drawing.Size(75, 23);
+            this.buttonVirgo.TabIndex = 5;
+            this.buttonVirgo.Text = "おとめ座";
+            this.buttonVirgo.UseVisualStyleBackColor = true;
+            this.buttonVirgo.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // buttonLibra
+            // 
+            this.buttonLibra.Location = new System.Drawing.Point(257, 162);
+            this.buttonLibra.Name = "buttonLibra";
+            this.buttonLibra.Size = new System.Drawing.Size(75, 23);
+            this.buttonLibra.TabIndex = 6;
+            this.buttonLibra.Text = "てんびん座";
+            this.buttonLibra.UseVisualStyleBackColor = true;
+            this.buttonLibra.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // buttonScorpio
+            // 
+            this.buttonScorpio.Location = new System.Drawing.Point(375, 162);
+            this.buttonScorpio.Name = "buttonScorpio";
+            this.buttonScorpio.Size = new System.Drawing.Size(75, 23);
+            this.buttonScorpio.TabIndex = 7;
+            this.buttonScorpio.Text = "さそり座";
+            this.buttonScorpio.UseVisualStyleBackColor = true;
+            this.buttonScorpio.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // buttonSagittarius
+            // 
+            this.buttonSagittarius.Location = new System.Drawing.Point(22, 257);
+            this.buttonSagittarius.Name = "buttonSagittarius";
+            this.buttonSagittarius.Size = new System.Drawing.Size(75, 23);
+            this.buttonSagittarius.TabIndex = 8;
+            this.buttonSagittarius.Text = "いて座";
+            this.buttonSagittarius.UseVisualStyleBackColor = true;
+            this.buttonSagittarius.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // buttonCapricorn
+            // 
+            this.buttonCapricorn.Location = new System.Drawing.Point(142, 257);
+            this.buttonCapricorn.Name = "buttonCapricorn";
+            this.buttonCapricorn.Size = new System.Drawing.Size(75, 23);
+            this.buttonCapricorn.TabIndex = 9;
+            this.buttonCapricorn.Text = "やぎ座";
+            this.buttonCapricorn.UseVisualStyleBackColor = true;
+            this.buttonCapricorn.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // buttonAquarius
+            // 
+            this.buttonAquarius.Location = new System.Drawing.Point(257, 257);
+            this.buttonAquarius.Name = "buttonAquarius";
+            this.buttonAquarius.Size = new System.Drawing.Size(75, 23);
+            this.buttonAquarius.TabIndex = 10;
+            this.buttonAquarius.Text = "みずがめ座";
+            this.buttonAquarius.UseVisualStyleBackColor = true;
+            this.buttonAquarius.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // buttonPisces
+            // 
+            this.buttonPisces.Location = new System.Drawing.Point(375, 257);
+            this.buttonPisces.Name = "buttonPisces";
+            this.buttonPisces.Size = new System.Drawing.Size(75, 23);
+            this.buttonPisces.TabIndex = 11;
+            this.buttonPisces.Text = "うお座";
+            this.buttonPisces.UseVisualStyleBackColor = true;
+            this.buttonPisces.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(129, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.TabIndex = 13;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(364, 106);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox3.TabIndex = 14;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(246, 106);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox4.TabIndex = 15;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(129, 106);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox5.TabIndex = 16;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(12, 106);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox6.TabIndex = 17;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Location = new System.Drawing.Point(364, 12);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox7.TabIndex = 18;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Location = new System.Drawing.Point(246, 12);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox8.TabIndex = 19;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Location = new System.Drawing.Point(364, 201);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox9.TabIndex = 20;
+            this.pictureBox9.TabStop = false;
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Location = new System.Drawing.Point(246, 201);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox10.TabIndex = 21;
+            this.pictureBox10.TabStop = false;
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.Location = new System.Drawing.Point(129, 201);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox11.TabIndex = 22;
+            this.pictureBox11.TabStop = false;
+            // 
+            // pictureBox12
+            // 
+            this.pictureBox12.Location = new System.Drawing.Point(12, 201);
+            this.pictureBox12.Name = "pictureBox12";
+            this.pictureBox12.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox12.TabIndex = 23;
+            this.pictureBox12.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(133, 300);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 20);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "あなたの星座を選んでね";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(-3, 334);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(487, 124);
+            this.richTextBox1.TabIndex = 25;
+            this.richTextBox1.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox12);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonPisces);
+            this.Controls.Add(this.buttonAquarius);
+            this.Controls.Add(this.buttonCapricorn);
+            this.Controls.Add(this.buttonSagittarius);
+            this.Controls.Add(this.buttonScorpio);
+            this.Controls.Add(this.buttonLibra);
+            this.Controls.Add(this.buttonVirgo);
+            this.Controls.Add(this.buttonLeo);
+            this.Controls.Add(this.buttonCancer);
+            this.Controls.Add(this.buttonGemini);
+            this.Controls.Add(this.buttonTaurus);
+            this.Controls.Add(this.buttonAries);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonAries;
+        private System.Windows.Forms.Button buttonTaurus;
+        private System.Windows.Forms.Button buttonGemini;
+        private System.Windows.Forms.Button buttonCancer;
+        private System.Windows.Forms.Button buttonLeo;
+        private System.Windows.Forms.Button buttonVirgo;
+        private System.Windows.Forms.Button buttonLibra;
+        private System.Windows.Forms.Button buttonScorpio;
+        private System.Windows.Forms.Button buttonSagittarius;
+        private System.Windows.Forms.Button buttonCapricorn;
+        private System.Windows.Forms.Button buttonAquarius;
+        private System.Windows.Forms.Button buttonPisces;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.PictureBox pictureBox12;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Horoscope
+{
+    public partial class Form1 : Form
+    {
+        private int count = 0;
+        private string textNo = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            textNo = "01";
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            textNo = "02";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            textNo = "03";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            textNo = "04";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            textNo = "05";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            textNo = "06";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            textNo = "07";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            textNo = "08";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            textNo = "09";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            textNo = "10";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            textNo = "11";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            textNo = "12";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Newdivination div = new TokyoDiv();
+            switch (count)
+            {
+                case 0:
+                    timer1.Interval = 2000;
+                    label1.Text = div.Concentration(0);
+                    break;
+
+                case 1:
+                    label1.Text = div.Concentration(1);
+                    break;
+
+                case 2:
+                    label1.Text = div.Concentration(2);
+                    richTextBox1.Text = div.Divine(textNo);
+                    timer1.Enabled = false;
+                    count = -1;
+                    break;
+            }
+            count++;
+        }
+    }
+
+    public class Divination
+    {
+        public string Divine(string no)
+        {
+            string divmsg = "";
+            string file_name = "..\\..\\unsei" + no + ".txt";
+            StreamReader sr = new StreamReader(file_name);
+            string s;
+            while ((s = sr.ReadLine()) != null) 
+            {
+                divmsg = divmsg + s + "\n";
+            }
+            sr.Close();
+            return divmsg;
+        }
+
+    }
+
+    abstract public class Newdivination :Divination
+    {
+        abstract public string Concentration(int no);
+       
+    }
+    public class TokyoDiv:Newdivination
+    {
+        override public string Concentration(int no)
+        {
+            string[] msg = { "そうですねぇ。", "ふむふむ", "これが今日の運勢です。" };
+            return msg[no];
+        }
+    }
+    public class OsakaDiv : Newdivination
+    {
+        override public string Concentration(int no)
+        {
+            string[] msg = { "そうやねぇ。", "どうかなー", "今日の運勢はこんな感じやな。" };
+            return msg[no];
+        }
+    }
+}
